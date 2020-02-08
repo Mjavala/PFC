ae23f8a580c6cf0e6a54430b9f80d62092377cb0
Mjavala <mjabravo221@gmail.com>
2020-02-08 00:02:08 -0700

    first-commit

---
+++ layouts/default.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+
+        <nuxt-link to="/">
+          <v-btn 
+            color="primary"
+            class="mx-2">
+            Sign-up
+          </v-btn>
+        </nuxt-link>
+        <nuxt-link to="login">
+          <v-btn 
+            color="primary"
+            class="mx-2">
+            Login
+          </v-btn>
+        </nuxt-link>
+
+        <nuxt-link to="secret">
+          <v-btn 
+            color="secondary"
+            class="mx-2">
+            Dashboard
+          </v-btn>
+        </nuxt-link>
+
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      title: 'PFC'
+    }
+  }
+}
+</script>
